7a9881b82c6b506b9285405425b3cf91195eb27b
anyyaa <83037109+anyyaa@users.noreply.github.com>
2024-04-09 12:45:14 +0300

    Update main.c

--- task3/main.c
+++ task3/main.c
@@ -6,82 +6,75 @@
 #include <stdlib.h>
 #include "structs.h"
 #include <string.h>
+#include "initialization.h"
+#include "drawing.h"
+#define MAX_WINDOWS 3
 
-
-SDL_Surface* screenSurface = NULL;
+bool windowsOpened[MAX_WINDOWS] = { false };
 Menu menu;
-
 
 int main() {
     init("data.txt");
-    SDL_Window** windows = malloc(menu.windowsCounter * sizeof(SDL_Window*));
-    SDL_Renderer** renderers = malloc(menu.windowsCounter * sizeof(SDL_Renderer*));
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    if (!createWindows(windows, renderers)) {
-        free(windows);
-        free(renderers);
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Window* window = SDL_CreateWindow("Menu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 700, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     bool quit = false;
     int selectedButtonIndex = 0;
+    SDL_Event event;
 
     while (!quit) {
-        for (int i = 0; i < menu.windowsCounter; i++) {
-            SDL_Event event;
-            while (SDL_PollEvent(&event) != 0) {
-                if (event.type == SDL_QUIT) {
-                    quit = true;
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+            else if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_UP) {
+                    selectedButtonIndex--;
+                    if (selectedButtonIndex < 0) {
+                        selectedButtonIndex = menu.buttonsCounter - 1;
+                    }
                 }
-                else if (event.type == SDL_KEYDOWN) {
-                    
-                    if (event.key.keysym.sym == SDLK_UP) {
-                        selectedButtonIndex--;
-                        if (selectedButtonIndex < 0) {
-                            selectedButtonIndex = menu.buttonsCounter - 1;
-                        }
+                else if (event.key.keysym.sym == SDLK_DOWN) {
+                    selectedButtonIndex++;
+                    if (selectedButtonIndex >= menu.buttonsCounter) {
+                        selectedButtonIndex = 0;
                     }
-                    else if (event.key.keysym.sym == SDLK_DOWN) {
-                        selectedButtonIndex++;
-                        if (selectedButtonIndex >= menu.buttonsCounter) {
-                            selectedButtonIndex = 0;
-                        }
-                    }
-                    
-                    else if (event.key.keysym.sym == SDLK_RETURN) {
-                        Button* selectedButton = &menu.buttons[selectedButtonIndex];
-                        if (selectedButton->color.red == selectedButton->highlightColor.red &&
-                            selectedButton->color.green == selectedButton->highlightColor.green &&
-                            selectedButton->color.blue == selectedButton->highlightColor.blue) {
-                            selectedButton->color = selectedButton->originalColor;
-                        }
-                        else {
-                            selectedButton->color = selectedButton->highlightColor;
-                        }
+                }
+                else if (event.key.keysym.sym == SDLK_RETURN) {
+                    getPressed(selectedButtonIndex, windowsOpened);
+                    if (selectedButtonIndex == 2) {
+                        quit = true;
                     }
                 }
             }
-
-            SDL_SetRenderDrawColor(renderers[i], menu.windows[i].color.red, menu.windows[i].color.green, menu.windows[i].color.blue, 255);
-            SDL_RenderClear(renderers[i]);
-            drawButtonsForWindow(renderers[i], menu.buttons, menu.buttonsCounter, menu.windows[i].id, selectedButtonIndex);
-            SDL_RenderPresent(renderers[i]);
         }
+
+        SDL_SetRenderDrawColor(renderer, 150, 189, 215, 255);
+        SDL_RenderClear(renderer); 
+
+        getWindow(&menu, renderer, 0);
+        drawButtonsForWindow(renderer, menu.buttons, menu.buttonsCounter, menu.windows[0].id, selectedButtonIndex);
+
+        for (int i = 1; i < MAX_WINDOWS; i++) {
+            if (windowsOpened[i]) {
+                getWindow(&menu, renderer, i);
+                drawButtonsForWindow(renderer, menu.buttons, menu.buttonsCounter, menu.windows[i].id, selectedButtonIndex);
+            }
+        }
+                
+        SDL_RenderPresent(renderer);
     }
 
-    for (int i = 0; i < menu.windowsCounter; i++) {
-        SDL_DestroyRenderer(renderers[i]);
-        SDL_DestroyWindow(windows[i]);
-    }
-    free(windows);
-    free(renderers);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
+
 
